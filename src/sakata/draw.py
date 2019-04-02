270c8ffe694255b94d573b6c30d700bcf9eaff65
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-04-02 15:31:11 +0900

    add sanpei

--- src/sakata/draw.py
+++ src/sakata/draw.py
@@ -26,7 +26,7 @@ def main():
     del df['Unnamed: 0']
     del df['volume']
 
-    # カラム名を変更
+    # カラム名を変更 ,time,close,open,high,low,volume
     df.columns = ['t', 'c', 'o', 'h', 'l']
     df.head()
 
@@ -53,14 +53,28 @@ def main():
     df['upper'] = df['mean'] + (df['std'] * 2)
     df['lower'] = df['mean'] - (df['std'] * 2)
 
+    # ルールその1 C3 < lower
+    df['rule_1'] = 1
+    df.loc[(df['c3'] < df['lower']),'rule_1']=0
+
+    # ルールその2
+    df['rule_2'] = -1
+    df.loc[(df['o'] - df['c'] < 0) & (df['o1'] - df['c1'] < 0) & (df['o2'] - df['c2'] < 0),'rule_2']=0
+
     # 最初の19行を削除してインデックスをリセット
     df = df[19:]
     df = df.reset_index(drop=True)
     df.head()
 
+
+    print(df)
+
+    # # ルール1とルール2が該当するレコードを探す
+    # df[(df['rule_1'] == 1.0) & (df['rule_2'] == 1.0)][0:5]
+
     # ローソク足表示
-    ax = plt.subplot(1, 1, 1)
-    candle_temp = df[550:750]
+    ax = plt.subplot(2, 1, 1)
+    candle_temp = df[16073:16402]
     candle_temp = candle_temp.reset_index()
     candlestick2_ohlc(
         ax, candle_temp["o"], candle_temp["h"], candle_temp["l"],
@@ -70,9 +84,15 @@ def main():
     ax.plot(candle_temp['mean'])
     ax.plot(candle_temp['upper'])
     ax.plot(candle_temp['lower'])
-     
+
+    ax = plt.subplot(2, 1, 2)
+    ax.plot(candle_temp['rule_1'])
+    ax.plot(candle_temp['rule_2'])
+
+
     plt.savefig('my_figure.png')
     plt.show()
+
 
 
 if __name__ == "__main__":
