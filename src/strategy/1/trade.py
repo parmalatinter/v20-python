fb771f16d7488902b5d6cc75c4d114a01180ec63
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-04-17 10:51:00 +0900

    Create trade.py

---
+++ src/strategy/1/trade.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import subprocess
+import market.condition
+
+def main():
+	
+	condition = market.condition.Market()
+	if condition.get_is_opening():
+		print('true')
+		command = 'v20-transaction-get-all'
+		print(command)
+		res = subprocess.Popen(command, shell=True)
+		print(res)
+		# command = 'v20-golden-draw'
+		# print(command)
+		# res = subprocess.Popen(command, shell=True)
+		# print(res
+	else:
+		print('false')
+
+if __name__ == "__main__":
+    main()
