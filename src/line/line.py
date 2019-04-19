4926b7e2ecddc55e83956e5dedee5ad23170938d
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-04-19 10:58:24 +0900

    Create line.py

---
+++ src/line/line.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import requests
+
+class Line(object):
+
+	line_notify_api = 'https://notify-api.line.me/api/notify'
+	headers = {}
+
+	def __init__(self):
+		line_notify_token = '5sVBSc70L2t1QbjOPEgBVCRYCFCiUZhs4zUcJ0YMThM'
+		
+		headers = {'Authorization': 'Bearer ' + line_notify_token}  # 発行したトークン
+
+	def send(self, title, message):
+		args = dict(title=title, message=message)
+		send_str = '%(title)s\n%(message)s' % args
+
+		payload = {'message': send_str}
+		line_notify = requests.post(self.line_notify_api, data=payload, headers=self.headers)
+
+def main():
+    line = Line()
+    line.send('title', 'message')
+
+if __name__ == "__main__":
+    main()
