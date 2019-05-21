5d742d6a06ee53a533cb0d48660cfad2d7b3bd20
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-05-21 13:50:19 +0900

    Update account.py

--- src/strategy/account.py
+++ src/strategy/account.py
@@ -3,21 +3,23 @@
 import subprocess
 import line.line
 
-def get_account_detail():
-	command = 'v20-account-details'
-	res = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
-	res.wait()
+class Account():
 
-	print(command)
-	out, err = res.communicate()
-	return command + ' ' + out.decode('utf-8')
-	
+	def get_account_detail(self):
+		command = 'v20-account-details'
+		res = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
+		res.wait()
 
-def main():
+		print(command)
+		out, err = res.communicate()
+		return command + ' ' + out.decode('utf-8')
+		
 
-	_line = line.line.Line()
-	res = get_account_detail()
-	_line.send('Now details #', res )
+	def main(self):
+		_line = line.line.Line()
+		res = self.get_account_detail()
+		_line.send('Now details #', res )
 
 if __name__ == "__main__":
-    main()
+	account = Account()
+	account.main()
