a6bc369dfc778d47e15ffb703a50ef6d04bdd4c8
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-05-21 13:51:24 +0900

    Create file_utility.py

---
+++ src/file/file_utility.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+import os 
+from io import StringIO
+import drive.drive
+
+class File_utility():
+	drive_id = ''
+	header = ''
+	content = ''
+	filemame = ''
+	googleDrive = None
+
+	def __init__(self, filemame, drive_id):
+		self.drive_id = drive_id
+		self.filemame = filemame
+		self.googleDrive = drive.drive.Drive(self.drive_id)
+
+	def set_header(self, header = ''):
+		self.header = header
+
+	def set_contents(self, contents):
+
+		for text in contents:
+			self.content = self.content + text
+
+	def export_drive(self):
+		self.googleDrive.upload(self.filemame, self.header +self.content)
+
+	def get_string(self):
+		googleDrive = drive.drive.Drive(self.drive_id)
+		res = googleDrive.get_content_by_filename(self.filemame)
+		if res: 	
+			return StringIO(res.GetContentString())
+		return ''
