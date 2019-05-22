9117a2e92f2e4fb1ed2e67eeecb681cbe59e941e
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-05-22 17:32:10 +0900

    Update condition.py

--- src/market/condition.py
+++ src/market/condition.py
@@ -86,7 +86,7 @@ class Market(object):
 		if not is_eneble:
 			return False
 
-		_calender = calendar.get.Calender()
+		_calender = calender.get.Calender()
 		df = _calender.get_df()
 		if not _calender.in_danger_time(df):
 			return False
