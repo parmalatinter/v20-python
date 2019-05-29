0a12289dcb248c3aa43e704a7e6d3b9875d0fe57
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-05-29 16:39:13 +0900

    Create get.py

---
+++ src/trend/get.py
@@ -0,0 +1,35 @@
+import requests
+import pandas as pd
+import json
+
+
+class Trend():
+
+	def get(self):
+		# https://currency-strength.com/
+		res = requests.get("https://currency-strength.com/php/chart1d.json")
+
+		j = res.json()
+
+		# print(j)
+		df = pd.DataFrame(j[0]).tail(1)
+		usd = df['values'][df.index[0]][1]
+
+		df = pd.DataFrame(j[2]).tail(1)
+		jpy = df['values'][df.index[0]][1] 
+
+		print(usd)
+		print(jpy)
+
+		print(usd - jpy)
+
+		return (usd - jpy)
+
+
+
+def main():
+	trend = Trend()
+	trend.get()
+
+if __name__ == "__main__":
+	main()
