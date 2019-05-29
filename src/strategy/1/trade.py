8be513c60898770ac32b4b5d8ee8fba091bc9b4f
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-05-30 01:42:09 +0900

    Update trade.py

--- src/strategy/1/trade.py
+++ src/strategy/1/trade.py
@@ -117,8 +117,8 @@ def main():
 	_environ = strategy.environ.Environ()
 
 	instrument = _environ.get('instrument') if _environ.get('instrument') else "USD_JPY"
-	units = _environ.get('units') if int(_environ.get('units')) else 10
-	hours = float(_environ.get('hours')) if _environ.get('hours') else 3
+	units = int(_environ.get('units')) if _environ.get('units') else 10
+	hours = int(_environ.get('hours')) if _environ.get('hours') else 3
 	reduce_time = float(_environ.get('reduce_time')) if _environ.get('reduce_time') else 5
 	drive_id = _environ.get('drive_id') if _environ.get('drive_id') else '1A3k4a4u4nxskD-hApxQG-kNhlM35clSa'
 	now_dt = None
