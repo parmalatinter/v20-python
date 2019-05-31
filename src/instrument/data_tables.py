b12b23029f1b60c883af3ebf419c7f48e2d03135
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-05-31 17:39:13 +0900

    Update data_tables.py

--- src/instrument/data_tables.py
+++ src/instrument/data_tables.py
@@ -9,11 +9,13 @@ class Data_tables():
 	file_name=None
 	instrument=None
 	before_hours=None
+	granularity=None
 
-	def __init__(self, file_name, instrument, before_hours):
+	def __init__(self, file_name, instrument, before_hours, granularity):
 		self.file_name = file_name
 		self.instrument = instrument
 		self.before_hours = before_hours
+		self.granularity = granularity
 
 	def set(self):
 		os.environ['TZ'] = 'America/New_York'
@@ -22,14 +24,14 @@ class Data_tables():
 
 		dir_path = os.path.dirname(os.path.realpath(__file__))
 		os.chdir(dir_path)
-		args = dict(instrument=self.instrument, before=before, file_name=self.file_name)
-		command = ' v20-instrument-candles %(instrument)s --from-time="%(before)s" --granularity=M10 --file_name="%(file_name)s"' % args
+		args = dict(instrument=self.instrument, before=before, file_name=self.file_name, granularity =self.granularity)
+		command = ' v20-instrument-candles %(instrument)s --from-time="%(before)s" --granularity="%(granularity)s" --file_name="%(file_name)s"' % args
 		print(command)
 		res = subprocess.Popen(command, shell=True)
 		print(res)
 
 def main():
-	data_tables = Data_tables('candles.csv', 'USD_JPY', 24)
+	data_tables = Data_tables('candles.csv', 'USD_JPY', 24, "M10")
 	data_tables.set()
 
 if __name__ == "__main__":
