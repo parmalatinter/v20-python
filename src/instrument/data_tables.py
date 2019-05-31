2b319a48dc93d5f1bb62a8903a4c5c648a2334f2
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-05-31 17:34:46 +0900

    Update data_tables.py

--- src/instrument/data_tables.py
+++ src/instrument/data_tables.py
@@ -4,18 +4,33 @@ import os
 from datetime import datetime, timedelta
 from pytz import timezone
 
-def main():
-	os.environ['TZ'] = 'America/New_York'
-	before_date= datetime.today() + timedelta(hours=-24)
-	before = before_date.strftime('%Y-%m-%d %H:%M:%S')
+class Data_tables():
 
-	dir_path = os.path.dirname(os.path.realpath(__file__))
-	os.chdir(dir_path)
-	args = dict(instrument='USD_JPY', before=before)
-	command = ' v20-instrument-candles %(instrument)s --from-time="%(before)s" --granularity=M10' % args
-	print(command)
-	res = subprocess.Popen(command, shell=True)
-	print(res)
+	file_name=None
+	instrument=None
+	before_hours=None
+
+	def __init__(self, file_name, instrument, before_hours):
+		self.file_name = file_name
+		self.instrument = instrument
+		self.before_hours = before_hours
+
+	def set(self):
+		os.environ['TZ'] = 'America/New_York'
+		before_date= datetime.today() + timedelta(hours=(0-self.before_hours))
+		before = before_date.strftime('%Y-%m-%d %H:%M:%S')
+
+		dir_path = os.path.dirname(os.path.realpath(__file__))
+		os.chdir(dir_path)
+		args = dict(instrument=self.instrument, before=before, file_name=self.file_name)
+		command = ' v20-instrument-candles %(instrument)s --from-time="%(before)s" --granularity=M10 --file_name="%(file_name)s"' % args
+		print(command)
+		res = subprocess.Popen(command, shell=True)
+		print(res)
+
+def main():
+	data_tables = Data_tables('candles.csv', 'USD_JPY', 24)
+	data_tables.set()
 
 if __name__ == "__main__":
     main()
