c633105cd0c36c813b9a9a69eafc4879024ab2ba
Yosuke <parmalatinter@gmail.com>
2019-06-03 23:30:49 +0900

    add trend caculate

--- src/instrument/candles_trend.py
+++ src/instrument/candles_trend.py
@@ -9,172 +9,86 @@ import os
 from pytz import timezone
 
 
+class Candles_trend():
+
+    def get(self):
+        """
+        Create an API context, and use it to fetch candles for an instrument.
+
+        The configuration for the context is parsed from the config file provided
+        as an argumentV
+        """
+
+        parser = argparse.ArgumentParser()
+
+        #
+        # The config object is initialized by the argument parser, and contains
+        # the REST APID host, port, accountID, etc.
+        #
+        common.config.add_argument(parser)
+
+        args = parser.parse_args()
+
+        account_id = args.config.active_account
+
+        #
+        # The v20 config object creates the v20.Context for us based on the
+        # contents of the config file.
+        #
+        api = args.config.create_context()
+
+        os.environ['TZ'] = 'America/New_York'
+        before_date= datetime.today() + timedelta(hours=-24)
+
+        weekday = before_date.weekday()
+        if(weekday > 5):
+            before_date= datetime.today() + timedelta(hours=-66)
+
+        kwargs = {}
+        kwargs["granularity"] = "H6"
+        kwargs["fromTime"] = api.datetime_to_str(before_date)
+
+        #
+        # Fetch the candles
+        #
+        response_list = [
+            {'key':'EUR_USD', 'value' : None},
+            {'key':'USD_JPY', 'value' : None},
+            {'key':'USD_CHF', 'value' : None},
+            {'key':'GBP_USD', 'value' : None},
+            {'key':'AUD_USD', 'value' : None},
+            {'key':'USD_CAD', 'value' : None},
+            {'key':'NZD_USD', 'value' : None},
+            {'key':'EUR_JPY', 'value' : None},
+            {'key':'CHF_JPY', 'value' : None},
+            {'key':'GBP_JPY', 'value' : None},
+            {'key':'AUD_JPY', 'value' : None},
+            {'key':'CAD_JPY', 'value' : None},
+            {'key':'NZD_JPY', 'value' : None}
+        ]
+
+        printer = CandlePrinter()
+        file_name = 'trend.csv'
+
+        res = '{},{},{},{},{},{},{},{}\n'.format("instrument", "index", "time", "close", "open", "high", "low", "volume")
+        for response in response_list:
+            response['value'] = api.instrument.candles(response['key'], **kwargs)
+            count = 0 
+            if response['value'].status != 200:
+                print(response['value'])
+                print(response['value'].body)
+                return
+
+            for candle in response['value'].get("candles", 200):
+                text = printer.get_format_csv(candle,file_name,count)
+                res=res+response['key'] + ',' + text
+                count=count+1
+
+        return res
+
 def main():
-    """
-    Create an API context, and use it to fetch candles for an instrument.
-
-    The configuration for the context is parsed from the config file provided
-    as an argumentV
-    """
-
-    parser = argparse.ArgumentParser()
-
-    #
-    # The config object is initialized by the argument parser, and contains
-    # the REST APID host, port, accountID, etc.
-    #
-    common.config.add_argument(parser)
-
-    parser.add_argument(
-        "--mid", 
-        action='store_true',
-        help="Get midpoint-based candles"
-    )
-
-    parser.add_argument(
-        "--bid", 
-        action='store_true',
-        help="Get bid-based candles"
-    )
-
-    parser.add_argument(
-        "--ask", 
-        action='store_true',
-        help="Get ask-based candles"
-    )
-
-    parser.add_argument(
-        "--smooth", 
-        action='store_true',
-        help="'Smooth' the candles"
-    )
-
-    parser.set_defaults(mid=False, bid=False, ask=False)
-
-    parser.add_argument(
-        "--count",
-        default=None,
-        help="The number of candles to fetch"
-    )
-
-    date_format = "%Y-%m-%d %H:%M:%S"
-
-    parser.add_argument(
-        "--from-time",
-        default=None,
-        type=common.args.date_time(),
-        help="The start date for the candles to be fetched. Format is 'YYYY-MM-DD HH:MM:SS'"
-    )
-
-    parser.add_argument(
-        "--to-time",
-        default=None,
-        type=common.args.date_time(),
-        help="The end date for the candles to be fetched. Format is 'YYYY-MM-DD HH:MM:SS'"
-    )
-
-    parser.add_argument(
-        "--alignment-timezone",
-        default=None,
-        help="The timezone to used for aligning daily candles"
-    )
-
-    parser.add_argument(
-        "--file_name",
-        default=None,
-        help="file_name'"
-    )
-
-    args = parser.parse_args()
-
-    account_id = args.config.active_account
-
-    #
-    # The v20 config object creates the v20.Context for us based on the
-    # contents of the config file.
-    #
-    api = args.config.create_context()
-
-    os.environ['TZ'] = 'America/New_York'
-    before_date= datetime.today() + timedelta(hours=-24)
-    before = before_date.strftime('%Y-%m-%d %H:%M:%S')
-
-    weekday = before_date.weekday()
-    if(weekday > 5):
-        diff = weekday - 3
-        before_date= datetime.today() + timedelta(hours=(-24*diff))
-
-    kwargs = {}
-
-    kwargs["granularity"] = "H12"
-    kwargs["fromTime"] = api.datetime_to_str(before_date)
-
-    if args.smooth is not None:
-        kwargs["smooth"] = args.smooth
-
-    if args.count is not None:
-        kwargs["count"] = args.count
-
-    if args.to_time is not None:
-        kwargs["toTime"] = api.datetime_to_str(args.to_time)
-
-    if args.alignment_timezone is not None:
-        kwargs["alignmentTimezone"] = args.alignment_timezone
-
-    price = "mid"
-
-    if args.mid:
-        kwargs["price"] = "M" + kwargs.get("price", "")
-        price = "mid"
-
-    if args.bid:
-        kwargs["price"] = "B" + kwargs.get("price", "")
-        price = "bid"
-
-    if args.ask:
-        kwargs["price"] = "A" + kwargs.get("price", "")
-        price = "ask"
-
-    #
-    # Fetch the candles
-    #
-    response_list = [
-        {'key':'EUR_USD', 'value' : None},
-        {'key':'USD_JPY', 'value' : None},
-        {'key':'USD_CHF', 'value' : None},
-        {'key':'GBP_USD', 'value' : None},
-        {'key':'AUD_USD', 'value' : None},
-        {'key':'USD_CAD', 'value' : None},
-        {'key':'NZD_USD', 'value' : None},
-        {'key':'EUR_JPY', 'value' : None},
-        {'key':'CHF_JPY', 'value' : None},
-        {'key':'GBP_JPY', 'value' : None},
-        {'key':'AUD_JPY', 'value' : None},
-        {'key':'CAD_JPY', 'value' : None},
-        {'key':'NZD_JPY', 'value' : None}
-    ]
-
-    printer = CandlePrinter()
-    file_name = 'trend.csv'
-    if args.file_name is not None:
-       file_name = args.file_name
-
-    res = '{},{},{},{},{},{},{},{}\n'.format("instrument", "index", "time", "close", "open", "high", "low", "volume")
-    for response in response_list:
-        response['value'] = api.instrument.candles(response['key'], **kwargs)
-        count = 0 
-        if response['value'].status != 200:
-            print(response['value'])
-            print(response['value'].body)
-            return
-
-        for candle in response['value'].get("candles", 200):
-            text = printer.get_format_csv(candle,file_name,count)
-            res=res+response['key'] + ',' + text
-            count=count+1
-
-    printer.export_drive(file_name, res)
-    
+    candles_trend = Candles_trend()
+    print(candles_trend.get())
 
 if __name__ == "__main__":
     main()
--- src/trend/get.py
+++ src/trend/get.py
@@ -3,41 +3,74 @@ import pandas as pd
 import json
 import math
 import instrument.data_tables
+import instrument.candles_trend
+import numpy as np
 
 class Trend():
-
-	# def __init__(self):
-	# 	instrument.data_tables.Data_tables('EURUSD_H1.csv', 'EUR_USD', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('USDJPY_H1.csv', 'USD_JPY', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('USDCHF_H1.csv', 'USD_CHF', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('GBPUSD_H1.csv', 'GBP_USD', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('AUDUSD_H1.csv', 'AUD_USD', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('USDCAD_H1.csv', 'USD_CAD', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('NZDUSD_H1.csv', 'NZD_USD', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('EURJPY_H1.csv', 'EUR_JPY', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('CHFJPY_H1.csv', 'CHF_JPY', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('GBPJPY_H1.csv', 'GBP_JPY', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('AUDJPY_H1.csv', 'AUD_JPY', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('CADJPY_H1.csv', 'CAD_JPY', 24, "H1").set()
-	# 	instrument.data_tables.Data_tables('NZDJPY_H1.csv', 'NZD_JPY', 24, "H1").set()
 	
 	def get(self):
 		# https://currency-strength.com/
-		res = requests.get("https://currency-strength.com/php/chart1d.json")
 
-		j = res.json()
+		candles_trend = instrument.candles_trend.Candles_trend()
 
-		df = pd.DataFrame(j[0]).tail(1)
-		usd = df['values'][df.index[0]][1]
+		csv = candles_trend.get()
 
-		df = pd.DataFrame(j[2]).tail(1)
-		jpy = df['values'][df.index[0]][1] 
-		
-		print(usd - jpy)
-		return (usd - jpy)
+		response_list = [
+			{'EUR_USD' : {'val' : None, 'res' : None}},
+			{'USD_JPY' : {'val' : None, 'res' : None}},
+			{'USD_CHF' : {'val' : None, 'res' : None}},
+			{'GBP_USD' : {'val' : None, 'res' : None}},
+			{'AUD_USD' : {'val' : None, 'res' : None}},
+			{'USD_CAD' : {'val' : None, 'res' : None}},
+			{'NZD_USD' : {'val' : None, 'res' : None}},
+			{'EUR_JPY' : {'val' : None, 'res' : None}},
+			{'CHF_JPY' : {'val' : None, 'res' : None}},
+			{'GBP_JPY' : {'val' : None, 'res' : None}},
+			{'AUD_JPY' : {'val' : None, 'res' : None}},
+			{'CAD_JPY' : {'val' : None, 'res' : None}},
+			{'NZD_JPY' : {'val' : None, 'res' : None}}
+		]
+
+		for response in response_list:
+
+			for property in list(response.keys()):
+
+				df = pd.read_csv(pd.compat.StringIO(csv), sep=',', engine='python', skipinitialspace=True)
+				dfs1 = df[(df['instrument'] == property)]
+				index = dfs1.shape[0] -1
+
+				# v1は起点の価格、v2は現在時刻の価格
+				response[property]['val'] = {'v1': dfs1['close'].values[0], 'v2': dfs1['close'].values[index]}
+
+		# v1は起点の価格、v2は現在時刻の価格
+		EURUSD = self.getVal(response_list[0]['EUR_USD']['val']['v1'],response_list[0]['EUR_USD']['val']['v2']);
+		USDJPY = self.getVal(response_list[1]['USD_JPY']['val']['v1'],response_list[1]['USD_JPY']['val']['v2']);
+		USDCHF = self.getVal(response_list[2]['USD_CHF']['val']['v1'],response_list[2]['USD_CHF']['val']['v2']);
+		GBPUSD = self.getVal(response_list[3]['GBP_USD']['val']['v1'],response_list[3]['GBP_USD']['val']['v2']);
+		AUDUSD = self.getVal(response_list[4]['AUD_USD']['val']['v1'],response_list[4]['AUD_USD']['val']['v2']);
+		USDCAD = self.getVal(response_list[5]['USD_CAD']['val']['v1'],response_list[5]['USD_CAD']['val']['v2']);
+		NZDUSD = self.getVal(response_list[6]['NZD_USD']['val']['v1'],response_list[6]['NZD_USD']['val']['v2']);
+		EURJPY = self.getValM(response_list[0]['EUR_USD']['val']['v1'],response_list[0]['EUR_USD']['val']['v2'],response_list[1]['USD_JPY']['val']['v1'],response_list[1]['USD_JPY']['val']['v2']);
+		EURCHF = self.getValM(response_list[0]['EUR_USD']['val']['v1'],response_list[0]['EUR_USD']['val']['v2'],response_list[2]['USD_CHF']['val']['v1'],response_list[2]['USD_CHF']['val']['v2']);
+		EURGBP = self.getValD(response_list[0]['EUR_USD']['val']['v1'],response_list[0]['EUR_USD']['val']['v2'],response_list[3]['GBP_USD']['val']['v1'],response_list[3]['GBP_USD']['val']['v2']);
+		CHFJPY = self.getValD(response_list[1]['USD_JPY']['val']['v1'],response_list[1]['USD_JPY']['val']['v2'],response_list[2]['USD_CHF']['val']['v1'],response_list[2]['USD_CHF']['val']['v2']);
+		GBPCHF = self.getValM(response_list[3]['GBP_USD']['val']['v1'],response_list[3]['GBP_USD']['val']['v2'],response_list[2]['USD_CHF']['val']['v1'],response_list[2]['USD_CHF']['val']['v2']);
+		GBPJPY = self.getValM(response_list[3]['GBP_USD']['val']['v1'],response_list[3]['GBP_USD']['val']['v2'],response_list[1]['USD_JPY']['val']['v1'],response_list[1]['USD_JPY']['val']['v2']);
+		AUDCHF = self.getValM(response_list[4]['AUD_USD']['val']['v1'],response_list[4]['AUD_USD']['val']['v2'],response_list[2]['USD_CHF']['val']['v1'],response_list[2]['USD_CHF']['val']['v2']);
+		AUDJPY = self.getValM(response_list[4]['AUD_USD']['val']['v1'],response_list[4]['AUD_USD']['val']['v2'],response_list[1]['USD_JPY']['val']['v1'],response_list[1]['USD_JPY']['val']['v2']);
+		CADJPY = self.getValD(response_list[1]['USD_JPY']['val']['v1'],response_list[1]['USD_JPY']['val']['v2'],response_list[5]['USD_CAD']['val']['v1'],response_list[5]['USD_CAD']['val']['v2']);
+		NZDJPY = self.getValM(response_list[6]['NZD_USD']['val']['v1'],response_list[6]['NZD_USD']['val']['v2'],response_list[1]['USD_JPY']['val']['v1'],response_list[1]['USD_JPY']['val']['v2']);
+
+		# # 各通貨の値の計算
+		Pairs = 7;
+		USD = (-EURUSD+USDJPY+USDCHF-GBPUSD-AUDUSD+USDCAD-NZDUSD)/Pairs;
+		JPY = (-EURJPY-USDJPY-CHFJPY-GBPJPY-AUDJPY-CADJPY-NZDJPY)/Pairs;
+
+		print(USD - JPY)
+		return (USD - JPY)
 
 	# 変化率
-	def getVal(v1, v2):
+	def getVal(self, v1, v2):
 
 		if v2 == 0:
 			return
@@ -45,7 +78,7 @@ class Trend():
 		return math.log(v1/v2)*10000
 
 
-	def getValM(v1, v2, v3, v4):
+	def getValM(self, v1, v2, v3, v4):
 
 		v1 = v1 * v3
 		v2 = v2 * v4
@@ -55,7 +88,7 @@ class Trend():
 		return math.log(v1/v2)*10000
 
 
-	def getValD(v1, v2, v3, v4):
+	def getValD(self, v1, v2, v3, v4):
 
 		if v3 == 0 or v4 == 0:
 			return
@@ -71,24 +104,7 @@ class Trend():
 def main():
 	trend = Trend()
 	trend.get()
-	# v1は起点の価格、v2は現在時刻の価格
-	# EURUSD = trend.getVal(v1['EURUSD'],v2['EURUSD']);
-	# USDJPY = trend.getVal(v1['USDJPY'],v2['USDJPY']);
-	# USDCHF = trend.getVal(v1['USDCHF'],v2['USDCHF']);
-	# GBPUSD = trend.getVal(v1['GBPUSD'],v2['GBPUSD']);
-	# AUDUSD = trend.getVal(v1['AUDUSD'],v2['AUDUSD']);
-	# USDCAD = trend.getVal(v1['USDCAD'],v2['USDCAD']);
-	# NZDUSD = trend.getVal(v1['NZDUSD'],v2['NZDUSD']);
-	# EURJPY = trend.getValM(v1['EURUSD'],v2['EURUSD'],v1['USDJPY'],v2['USDJPY']);
-	# EURCHF = trend.getValM(v1['EURUSD'],v2['EURUSD'],v1['USDCHF'],v2['USDCHF']);
-	# EURGBP = trend.getValD(v1['EURUSD'],v2['EURUSD'],v1['GBPUSD'],v2['GBPUSD']);
-	# CHFJPY = trend.getValD(v1['USDJPY'],v2['USDJPY'],v1['USDCHF'],v2['USDCHF']);
-	# GBPCHF = trend.getValM(v1['GBPUSD'],v2['GBPUSD'],v1['USDCHF'],v2['USDCHF']);
-	# GBPJPY = trend.getValM(v1['GBPUSD'],v2['GBPUSD'],v1['USDJPY'],v2['USDJPY']);
-	# AUDCHF = trend.getValM(v1['AUDUSD'],v2['AUDUSD'],v1['USDCHF'],v2['USDCHF']);
-	# AUDJPY = trend.getValM(v1['AUDUSD'],v2['AUDUSD'],v1['USDJPY'],v2['USDJPY']);
-	# CADJPY = trend.getValD(v1['USDJPY'],v2['USDJPY'],v1['USDCAD'],v2['USDCAD']);
-	# NZDJPY = trend.getValM(v1['NZDUSD'],v2['NZDUSD'],v1['USDJPY'],v2['USDJPY']);
+
 
 	# # 各通貨の値の計算
 	# Pairs = 7;
