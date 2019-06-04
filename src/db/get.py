65edcfe7fda3d42143372076123c4a89c24cc78f
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-06-04 20:19:25 +0900

    Update get.py

--- src/db/get.py
+++ src/db/get.py
@@ -28,7 +28,7 @@ class DB():
 
 		cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
 		cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
-		cur.execute("SELECT * FROM test;")
+		print(cur.execute("SELECT * FROM test;"))
 		cur.execute("DROP TABLE test")
 
 		cur.fetchone()
