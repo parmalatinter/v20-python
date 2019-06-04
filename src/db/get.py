8dd8ac0b946f560904abb5a75b9451f3991ab49f
parmalatinter <parmalatinter@gmail.com>
2019-06-04 18:21:51 +0900

    add db

--- src/db/get.py
+++ src/db/get.py
@@ -31,6 +31,7 @@ class DB():
 		cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
 		cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
 		cur.execute("SELECT * FROM test;")
+		cur.execute('DROP DATABASE IF EXISTS test')
 		cur.fetchone()
 		conn.commit()
 		cur.close()
