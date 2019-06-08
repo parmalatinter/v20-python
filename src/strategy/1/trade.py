e0ba472bbe6a9265a88fb32313815711dfe660c6
Yosuke <parmalatinter@gmail.com>
2019-06-08 09:54:42 +0900

    clean trade

--- src/strategy/1/trade.py
+++ src/strategy/1/trade.py
@@ -32,17 +32,19 @@ import copy
 class Trade():
 
 	is_ordered = False
-	drive_id = ''
 	history = db.history.History()
 	_line = line.line.Line()
+	instrument =  "USD_JPY"
+	units = 10
+	hours = 3
+	trend_usd = trend.get.Trend().get()
 
-	def init(self, drive_id):
-		self.is_ordered = False
-		self.drive_id = drive_id
+	def __init__(self, _environ):
 		os.environ['TZ'] = 'America/New_York'
-		googleDrive = drive.drive.Drive(self.drive_id)
-		googleDrive.delete_all()
-		time.sleep(5)
+		self.is_ordered = False
+		self.instrument = _environ.get('instrument') if _environ.get('instrument')
+		self.units = int(_environ.get('units')) if _environ.get('units') 
+		self.hours = int(_environ.get('hours')) if _environ.get('hours')
 
 	def get_df(self, csv_string):
 		return pd.read_csv(csv_string, sep=',', engine='python', skipinitialspace=True)
@@ -59,7 +61,7 @@ class Trade():
 		self._line.send('order #', command + ' ' + out.decode('utf-8') )
 		self.is_ordered = True
 
-	def close(self, instrument, df, hours, now_dt, last_rate):
+	def close(self, df, now_dt, last_rate):
 
 		now_dt = datetime.strptime(now_dt.replace('T', ' '), '%Y-%m-%d %H:%M:%S')
 
@@ -71,7 +73,7 @@ class Trade():
 			delta_total_minuts = delta.total_seconds()/60
 			delta_total_hours = delta_total_minuts/60
 
-			if delta_total_hours >= hours:
+			if delta_total_hours >= self.hours:
 				args = dict(tradeid=row.id, units='ALL')
 				command = ' v20-trade-close %(tradeid)s --units="%(units)s"' % args
 				res = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
@@ -86,12 +88,7 @@ class Trade():
 		details = account.get_account_detail()
 		return details
 
-	def exec_command(self, command):
-		res = subprocess.Popen(command, shell=True)
-		res.wait()
-		time.sleep(5)
-
-	def golden_trade(self, instrument, units, df_candles, trend_usd):
+	def golden_trade(self, df_candles):
 		draw = golden.draw.Draw()
 		df = draw.caculate(df_candles)
 		last_df = df.tail(1)
@@ -110,21 +107,21 @@ class Trade():
 
 		if is_golden:
 			is_golden = True
-			if trend_usd['res'] > 5:
+			if self.trend_usd['res'] > 5:
 				_message = ("buy order 1 #",str(late))
-				_units = units
+				_units = self.units
 				_event_open_id = 1
 				_target_price = late + 0.1
 				
-			elif trend_usd['res'] < -5:
+			elif self.trend_usd['res'] < -5:
 				_message = ("sell order 2 #",str(late))
-				_units = 0 - units
+				_units = 0 - self.units
 				_event_open_id = 2
 				_target_price = late - 0.1
 
 			else:
 				_message = ("buy order 3 #",str(late))
-				_units = units
+				_units = self.units
 				_event_open_id = 3
 				_target_price = late + 0.05
 		else:
@@ -132,21 +129,21 @@ class Trade():
 
 		if is_dead:
 			is_dead = True
-			if trend_usd['res'] < -5:
+			if self.trend_usd['res'] < -5:
 				_message = ("sell order 1 #",str(late))
-				_units = 0 - units
+				_units = 0 - self.units
 				_event_open_id = 4
 				_target_price = late - 0.1
 
-			elif trend_usd['res'] > 5:
+			elif self.trend_usd['res'] > 5:
 				_message = ("buy order 2 #",str(late))
-				_units = units
+				_units = self.units
 				_event_open_id = 5
 				_target_price = late + 0.1
 				
 			else:
 				_message = ("sell order 3 #",str(late))
-				_units = 0 - units
+				_units = 0 - self.units
 				_event_open_id = 6
 				_target_price = late - 0.05
 		else:
@@ -154,26 +151,25 @@ class Trade():
 
 		if rule_1 and rule_2:
 			_message = ("buy chance order #",str(late))
-			_units = units * 2
+			_units = self.units * 2
 			_event_open_id = 7
 			_target_price = late + 0.1
 
 		elif rule_3 and rule_4:
 			_message = ("sell chance order #",str(late))
-			_units = 0 - (units * 2)
+			_units = 0 - (self.units * 2)
 			_event_open_id = 8
 			_target_price = late - 0.1
 		
 		if _event_open_id > 0:
 			self.is_ordered = True
 			_target_price =  round(_target_price, 2)
-			self.order(instrument, _units,_target_price)
+			self.order(self.instrument, _units,_target_price)
 			self._line.send(_event_open_id, _message)
 
 			return {
 				'late': round(late, 2),
 				'target_price' : round(_target_price, 2),
-				'instrument': instrument,
 				'units': _units,
 				'event_open_id' : _event_open_id,
 				'is_golden': is_golden,
@@ -190,25 +186,25 @@ class Trade():
 		last_df = df.tail(1)
 		return {'time' : last_df['time'][last_df.index[0]], 'close' : last_df['close'][last_df.index[0]]}
 
-	def insert_histoy(self, trade_history, trade_id, trend_usd):
+	def insert_histoy(self, trade_history, trade_id):
 		self.history.insert(
 			int(trade_id),
 			float(trade_history['late']),
 			float(trade_history['target_price']),
 			'OPEN',
-			trade_history['instrument'],
+			self.instrument,
 			trade_history['units'],
 			0,
 			trade_history['event_open_id'],
-			round(trend_usd['v1'], 2),
-			round(trend_usd['v2'], 2),
+			round(self.trend_usd['v1'], 2),
+			round(self.trend_usd['v2'], 2),
 			trade_history['is_golden'],
 			trade_history['is_dead'],
 			trade_history['rule_1'],
 			trade_history['rule_2'],
 			trade_history['rule_3'],
 			trade_history['rule_4'],
-			round(trend_usd['res'], 2),
+			round(self.trend_usd['res'], 2),
 		)
 
 	def get_histoy_csv(self):
@@ -216,22 +212,21 @@ class Trade():
 	
 def main():
 
-	_environ = strategy.environ.Environ()
-
-	instrument = _environ.get('instrument') if _environ.get('instrument') else "USD_JPY"
-	units = int(_environ.get('units')) if _environ.get('units') else 10
-	hours = int(_environ.get('hours')) if _environ.get('hours') else 3
-	reduce_time = float(_environ.get('reduce_time')) if _environ.get('reduce_time') else 5
-	drive_id = _environ.get('drive_id') if _environ.get('drive_id') else '1A3k4a4u4nxskD-hApxQG-kNhlM35clSa'
-	trend_usd = trend.get.Trend().get()
-	now_dt = None
 	
 	condition = market.condition.Market()
 	if condition.get_is_opening() == False:
 		exit()
 
-	trade = Trade()
-	trade.init(drive_id)
+	_environ = strategy.environ.Environ()
+	reduce_time = float(_environ.get('reduce_time')) if _environ.get('reduce_time') else 5
+	drive_id = _environ.get('drive_id') if _environ.get('drive_id') else '1A3k4a4u4nxskD-hApxQG-kNhlM35clSa'
+	now_dt = None
+
+	googleDrive = drive.drive.Drive(drive_id)
+	googleDrive.delete_all()
+	time.sleep(5)
+
+	trade = Trade(_environ)
 	
 	candles = inst.Candles()
 	candles_csv_string= candles.get('USD_JPY', 'M10')
@@ -239,7 +234,7 @@ def main():
 
 	trade_history = None
 	if condition.get_is_eneble_new_order(reduce_time):
-		trade_history = trade.golden_trade(instrument, units, candles_df, trend_usd)
+		trade_history = trade.golden_trade(candles_df)
 		
 	info = trade.get_info(candles_df)
 
@@ -249,11 +244,11 @@ def main():
 
 	if not transaction_df.empty:
 		if info['time']:
-			trade.close(instrument, transaction_df, hours, info['time'], info['close'])
+			trade.close(transaction_df, hours, info['time'], info['close'])
 
 		if trade_history:
 			last_df = transaction_df.tail(1)
-			trade.insert_histoy(trade_history,last_df['id'][last_df.index[0]], trend_usd)
+			trade.insert_histoy(trade_history,last_df['id'][last_df.index[0]])
 
 	details = trade.get_account_details()
 	details_csv = file.file_utility.File_utility('details.csv', drive_id)
