46fffd8995019afc26c45dc5321db8f9df0a787c
parmalatinter <parmalatinter@gmail.com>
2019-06-19 18:18:22 +0900

    beautify code db

--- src/db/history.py
+++ src/db/history.py
@@ -1,258 +1,258 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT, register_adapter, AsIs
 import strategy.environ
-import os 
+import os
 import datetime
 import pandas as pd
 import numpy
 
+
 class History():
 
-	dbname ="dat1co0qkecvuj"
-	user = "kenxmeuwltvdre"
-	password = "ffd50671d51cb82f338f3a6c1d003e3ce7e921c675f9e0c248f526ff2e60750d"
-	host="ec2-23-21-148-223.compute-1.amazonaws.com"
-	port="5432"
-	dir_path = os.path.dirname(os.path.realpath(__file__))
+    dbname = "dat1co0qkecvuj"
+    user = "kenxmeuwltvdre"
+    password = "ffd50671d51cb82f338f3a6c1d003e3ce7e921c675f9e0c248f526ff2e60750d"
+    host = "ec2-23-21-148-223.compute-1.amazonaws.com"
+    port = "5432"
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-	def __init__(self):
-		_environ = strategy.environ.Environ()
-		self.dbname = _environ.get('dbname') if _environ.get('dbname') else self.dbname
-		self.user = _environ.get('user') if _environ.get('user') else self.user
-		self.password = _environ.get('password') if _environ.get('password') else self.password
-		self.host = _environ.get('host') if _environ.get('host') else self.host
-		self.port = _environ.get('port') if _environ.get('port') else self.port
+    def __init__(self):
+        _environ = strategy.environ.Environ()
+        self.dbname = _environ.get('dbname') if _environ.get(
+            'dbname') else self.dbname
+        self.user = _environ.get('user') if _environ.get('user') else self.user
+        self.password = _environ.get('password') if _environ.get(
+            'password') else self.password
+        self.host = _environ.get('host') if _environ.get('host') else self.host
+        self.port = _environ.get('port') if _environ.get('port') else self.port
 
-	def get_conn(self):
-		conn = psycopg2.connect("host=" + self.host + " port=" + self.port + " dbname=" + self.dbname + " user=" + self.user + " password=" + self.password)
-		conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    def get_conn(self):
+        conn = psycopg2.connect("host=" + self.host + " port=" + self.port + " dbname=" +
+                                self.dbname + " user=" + self.user + " password=" + self.password)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
-		return conn
+        return conn
 
-	def exec_query(self, query, args = None, is_get_res = False):
-		conn = self.get_conn()
+    def exec_query(self, query, args=None, is_get_res=False):
+        conn = self.get_conn()
 
-		cur = conn.cursor()
-		
-		cur.execute(query, args)
+        cur = conn.cursor()
 
-		res = None
+        cur.execute(query, args)
 
-		if is_get_res:
-			res = cur.fetchall()
-		
-		conn.commit()
-		cur.close()
-		conn.close()
+        res = None
 
-		return res
+        if is_get_res:
+            res = cur.fetchall()
 
-	def exec_query_by_panda(self, query, index_col):
-		conn = self.get_conn()
+        conn.commit()
+        cur.close()
+        conn.close()
 
-		cur = conn.cursor()
+        return res
 
-		res = pd.read_sql(sql=query, con=conn, index_col=index_col )
+    def exec_query_by_panda(self, query, index_col):
+        conn = self.get_conn()
 
-		conn.commit()
-		cur.close()
-		conn.close()
+        cur = conn.cursor()
 
-		return res
+        res = pd.read_sql(sql=query, con=conn, index_col=index_col)
 
-	def get(self):
+        conn.commit()
+        cur.close()
+        conn.close()
 
-		return self.exec_query("SELECT * FROM history ORDER BY trade_id;", None, True)
+        return res
 
-	def get_trade_ids_by_not_update_pl_by_panda(self):
+    def get(self):
 
-		conn = self.get_conn()
+        return self.exec_query("SELECT * FROM history ORDER BY trade_id;", None, True)
 
-		cur = conn.cursor()
+    def get_trade_ids_by_not_update_pl_by_panda(self):
 
-		query = "SELECT trade_id FROM history where unrealized_pl = 0.0 OR pl IS NULL ;"
-		rows = self.exec_query_by_panda(query, 'trade_id')
+        conn = self.get_conn()
 
-		cur.close()
-		conn.close()
+        cur = conn.cursor()
 
-		return rows.index.values
+        query = "SELECT trade_id FROM history where unrealized_pl = 0.0 OR pl IS NULL ;"
+        rows = self.exec_query_by_panda(query, 'trade_id')
 
-	def get_by_panda(self, trade_id):
-		conn = self.get_conn()
+        cur.close()
+        conn.close()
 
-		cur = conn.cursor()
+        return rows.index.values
 
-		args = dict(trade_id=trade_id)
-		query = 'SELECT * FROM history where trade_id = %(trade_id)s;' % args
-		rows = self.exec_query_by_panda(query, 'id')
+    def get_by_panda(self, trade_id):
+        conn = self.get_conn()
 
-		cur.close()
-		conn.close()
+        cur = conn.cursor()
 
-		return rows.tail(1)
+        args = dict(trade_id=trade_id)
+        query = 'SELECT * FROM history where trade_id = %(trade_id)s;' % args
+        rows = self.exec_query_by_panda(query, 'id')
 
-	def get_all_by_panda(self):
-		conn = self.get_conn()
+        cur.close()
+        conn.close()
 
-		cur = conn.cursor()
+        return rows.tail(1)
 
-		sql_file = open(self.dir_path + '/query/history/select_all.sql','r')
+    def get_all_by_panda(self):
+        conn = self.get_conn()
 
-		rows = self.exec_query_by_panda(sql_file.read(), 'id')
+        cur = conn.cursor()
 
-		cur.close()
-		conn.close()
+        sql_file = open(self.dir_path + '/query/history/select_all.sql', 'r')
 
-		return rows
+        rows = self.exec_query_by_panda(sql_file.read(), 'id')
 
+        cur.close()
+        conn.close()
 
-	def get_all_by_panda2(self):
-		conn = self.get_conn()
+        return rows
 
-		cur = conn.cursor()
+    def get_all_by_panda2(self):
+        conn = self.get_conn()
 
-		rows = self.exec_query_by_panda("SELECT state , event_open_id, count(state), max(state) FROM history GROUP BY state, event_open_id ORDER BY state", 'state')
+        cur = conn.cursor()
 
-		cur.close()
-		conn.close()
+        rows = self.exec_query_by_panda(
+            "SELECT state , event_open_id, count(state), max(state) FROM history GROUP BY state, event_open_id ORDER BY state", 'state')
 
-		return rows
+        cur.close()
+        conn.close()
 
-	def get_all_by_csv(self):
-		df = self.get_all_by_panda()
+        return rows
 
-		return df.to_csv(sep=",", line_terminator='\n', encoding='utf-8')
+    def get_all_by_csv(self):
+        df = self.get_all_by_panda()
 
-	def insert(self, trade_id, price, price_target, state, instrument, units, unrealized_pl, event_open_id, trend_1, trend_2, trend_3, trend_4, trend_cal, judge_1, judge_2, rule_1, rule_2, rule_3, rule_4, rule_5, rule_6, memo=''):
-		create_time = datetime.datetime.now() 
-		
-		sql_file = open(self.dir_path + '/query/history/insert.sql','r')
-		args =(
-			trade_id,
-			create_time,
-			price,
-			price_target,
-			state,
-			instrument,
-			units,
-			unrealized_pl,
-			event_open_id,
-			trend_1,
-			trend_2,
-			trend_3,
-			trend_4,
-			trend_cal,
-			judge_1,
-			judge_2,
-			rule_1,
-			rule_2,
-			rule_3,
-			rule_4,
-			rule_5,
-			rule_6,
-			memo
-		)
-		self.exec_query( sql_file.read(), args)
+        return df.to_csv(sep=",", line_terminator='\n', encoding='utf-8')
 
+    def insert(self, trade_id, price, price_target, state, instrument, units, unrealized_pl, event_open_id, trend_1, trend_2, trend_3, trend_4, trend_cal, judge_1, judge_2, rule_1, rule_2, rule_3, rule_4, rule_5, rule_6, memo=''):
+        create_time = datetime.datetime.now()
 
-	def delete(self, trade_id):
-		create_time = datetime.datetime.now() 
-		
-		query = 'DELETE FROM history where trade_id = %s'
+        sql_file = open(self.dir_path + '/query/history/insert.sql', 'r')
+        args = (
+            trade_id,
+            create_time,
+            price,
+            price_target,
+            state,
+            instrument,
+            units,
+            unrealized_pl,
+            event_open_id,
+            trend_1,
+            trend_2,
+            trend_3,
+            trend_4,
+            trend_cal,
+            judge_1,
+            judge_2,
+            rule_1,
+            rule_2,
+            rule_3,
+            rule_4,
+            rule_5,
+            rule_6,
+            memo
+        )
+        self.exec_query(sql_file.read(), args)
 
-		self.exec_query( query, (trade_id,))
+    def delete(self, trade_id):
 
-	def update(self, trade_id, price_close, pl, event_close_id, state):
+        query = 'DELETE FROM history where trade_id = %s'
 
-		update_time = datetime.datetime.now() 
-		
-		sql_file = open(self.dir_path + '/query/history/update.sql','r')
-		self.exec_query(sql_file.read(),(update_time, price_close, pl, event_close_id, state, trade_id))
+        self.exec_query(query, (trade_id,))
 
-	def fix_update(self, trade_id, createTime, filledTime, price_close, pl, memo=''):
-		
-		sql_file = open(self.dir_path + '/query/history/fix_update.sql','r')
-		self.exec_query(sql_file.read(),(createTime, filledTime, price_close, pl, memo, trade_id))
+    def update(self, trade_id, price_close, pl, event_close_id, state):
 
-	def create(self):
-		sql_file = open(self.dir_path + '/query/history/create.sql','r')
-		self.exec_query(sql_file.read())
+        update_time = datetime.datetime.now()
 
-	def add_column(self, column_name_type):
-		sql_file = open(self.dir_path + '/query/history/add_column.sql','r')
-		self.exec_query('ALTER TABLE history ADD COLUMN IF NOT EXISTS ' + column_name_type)
+        sql_file = open(self.dir_path + '/query/history/update.sql', 'r')
+        self.exec_query(sql_file.read(), (update_time,
+                                          price_close, pl, event_close_id, state, trade_id))
 
-	def drop(self):
+    def fix_update(self, trade_id, createTime, filledTime, price_close, pl, memo=''):
 
-		self.exec_query( 'DROP TABLE IF EXISTS history')
+        sql_file = open(self.dir_path + '/query/history/fix_update.sql', 'r')
+        self.exec_query(sql_file.read(), (createTime,
+                                          filledTime, price_close, pl, memo, trade_id))
+
+    def create(self):
+        sql_file = open(self.dir_path + '/query/history/create.sql', 'r')
+        self.exec_query(sql_file.read())
+
+    def add_column(self, column_name_type):
+        sql_file = open(self.dir_path + '/query/history/add_column.sql', 'r')
+        self.exec_query(
+            'ALTER TABLE history ADD COLUMN IF NOT EXISTS ' + column_name_type)
+
+    def drop(self):
+
+        self.exec_query('DROP TABLE IF EXISTS history')
+
 
 def main():
-	history = History()
-	print(history.get_trade_ids_by_not_update_pl_by_panda())
-	# history.add_column("trend_3 numeric")
-	# history.add_column("trend_4 numeric")
-	# history.add_column("trend_cal numeric")
-	# history.add_column("rule_5 boolean")
-	# history.add_column("rule_6 boolean")
-	# history.drop()
+    history = History()
+    print(history.get_trade_ids_by_not_update_pl_by_panda())
+    # history.add_column("trend_3 numeric")
+    # history.add_column("trend_4 numeric")
+    # history.add_column("trend_cal numeric")
+    # history.add_column("rule_5 boolean")
+    # history.add_column("rule_6 boolean")
+    # history.drop()
 
-	# history.create()
-	# trade_id = 1
-	# price = 100.20
-	# price_target = 100.30
-	# state = 'state 1'
-	# instrument = 'USD_JPY'
-	# units = 10000
-	# unrealized_pl = 10000
-	# event_open_id = 1
-	# trend_1 = 10 
-	# trend_2 = 20 
-	# judge_1 = True 
-	# judge_2 = False
-	# memo = 'test'
-	# rule_1 = True
-	# rule_2 = True
-	# rule_3 = True
-	# rule_4 = True
+    # history.create()
+    # trade_id = 1
+    # price = 100.20
+    # price_target = 100.30
+    # state = 'state 1'
+    # instrument = 'USD_JPY'
+    # units = 10000
+    # unrealized_pl = 10000
+    # event_open_id = 1
+    # trend_1 = 10
+    # trend_2 = 20
+    # judge_1 = True
+    # judge_2 = False
+    # memo = 'test'
+    # rule_1 = True
+    # rule_2 = True
+    # rule_3 = True
+    # rule_4 = True
 
-	# history.insert(
-	# 	trade_id,
-	# 	price,
-	# 	price_target,
-	# 	state,
-	# 	instrument,
-	# 	units,
-	# 	unrealized_pl,
-	# 	event_open_id,
-	# 	trend_1,
-	# 	trend_2,
-	# 	judge_1,
-	# 	judge_2,
-	# 	rule_1,
-	# 	rule_2,
-	# 	rule_3,
-	# 	rule_4,
-	# 	memo
-	# )
+    # history.insert(
+    # 	trade_id,
+    # 	price,
+    # 	price_target,
+    # 	state,
+    # 	instrument,
+    # 	units,
+    # 	unrealized_pl,
+    # 	event_open_id,
+    # 	trend_1,
+    # 	trend_2,
+    # 	judge_1,
+    # 	judge_2,
+    # 	rule_1,
+    # 	rule_2,
+    # 	rule_3,
+    # 	rule_4,
+    # 	memo
+    # )
 
-	# pl = 20000
-	# price_close = 100.40
-	# event_close_id = 1
-	# history.update(trade_id, price_close, pl, event_close_id, state)
+    # pl = 20000
+    # price_close = 100.40
+    # event_close_id = 1
+    # history.update(trade_id, price_close, pl, event_close_id, state)
 
-	# # print(history.get_all_by_panda())
-	# df = history.get_by_panda(1)
-	# # print(df['trade_id'][df.index[0]])
+    # # print(history.get_all_by_panda())
+    # df = history.get_by_panda(1)
+    # # print(df['trade_id'][df.index[0]])
 
-	# history.delete(1364)
+    # history.delete(1364)
 
-	
+
 if __name__ == "__main__":
-	main()
-
-
-
-
-
-
+    main()
