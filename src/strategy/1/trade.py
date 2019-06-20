175252ee0c1bb72477f0c33e035c8d98ee1ec91c
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-06-20 12:58:25 +0900

    Update trade.py

--- src/strategy/1/trade.py
+++ src/strategy/1/trade.py
@@ -356,7 +356,6 @@ class Trade():
                 # 90分 ~ で利益ない場合　とりあえず発注価格でcloseする
                 event_close_id = 7
 
-                self.stop_loss(trade_id, round(stop_rate, 2), stopLossOrderID, client_order_comment, event_close_id)
                 self.take_profit(trade_id, price, takeProfitOrderID, client_order_comment, event_close_id)
                 self.history.update(int(trade_id), last_rate,  float(
                     row['unrealizedPL']), event_close_id, state)                                 
