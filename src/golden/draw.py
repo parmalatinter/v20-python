a436df1bb949b9f022c3ff5d79088561deb8b3ed
Yosuke <parmalatinter@gmail.com>
2019-07-02 21:17:51 +0900

    fix band calculate

--- src/golden/draw.py
+++ src/golden/draw.py
@@ -166,10 +166,10 @@ class Draw(object):
         df['lower_low'] = df['mean'] - (df['std'] * 2)
 
         # 期間5単純移動平均
-        df['sma_5'] = np.round(df['c'].rolling(window=5).mean(), 2)
-        # 期間15単純移動平均
-        df['sma_15'] = np.round(df['c'].rolling(window=15).mean(), 2)
-        df['diff'] = df['sma_5'] - df['sma_15']
+        df['sma_4'] = np.round(df['c'].rolling(window=5).mean(), 2)
+        # 期間14単純移動平均
+        df['sma_14'] = np.round(df['c'].rolling(window=15).mean(), 2)
+        df['diff'] = df['sma_5'] - df['sma_14']
 
         # ルールその1 C3 < lower
         df['rule_1'] = 0
