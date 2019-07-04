4ec5ca28732f09303d85abe71fac13aff9a3fae0
Yosuke <parmalatinter@gmail.com>
2019-07-04 23:27:01 +0900

    add env to line

--- src/line/line.py
+++ src/line/line.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
 import requests
+import strategy.environ
 
 class Line(object):
 
 	line_notify_api = 'https://notify-api.line.me/api/notify'
+	# テスト
+	line_notify_token = 'H2dgkYyK6FYQo4GZo21MLR50ObjZmkv9dkxYEZc78lk' 
+	env = 'TEST'
 	headers = {}
 
 	def __init__(self):
-		line_notify_token = 'H2dgkYyK6FYQo4GZo21MLR50ObjZmkv9dkxYEZc78lk'
+		environ = strategy.environ.Environ()
+		self.env = environ.get('ENV') if environ.get('ENV') else self.env
+		if not self.env == 'PRO':
+			# 本番
+			self.line_notify_token = 'CBmYadvZxpKXCWZ7hFuFn1BwkPkClacjlWujm5y9ulQ'
 		
-		self.headers = {'Authorization': 'Bearer ' + line_notify_token}  # 発行したトークン
+		self.headers = {'Authorization': 'Bearer ' + self.line_notify_token}  # 発行したトークン
 
 	def send(self, title, message):
 		args = dict(title=title, message=message)
