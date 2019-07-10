b0425eadca0374036c4d1336be52348bea83a658
parmalatinter <parmalatinter@gmail.com>
2019-07-10 16:19:13 +0900

    add stock market to calender

--- src/calender/get.py
+++ src/calender/get.py
@@ -71,30 +71,30 @@ class Calendar(object):
         dfs1 = dfs1.drop("us_date", axis=1)
         dfs1 = dfs1.drop("base_us_datetime", axis=1)
 
-        # 日経開始時間
-        us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 20:00'
-        from_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 19:00'
-        to_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 21:00'
-        time = datetime.time(9, 00, 00)
-        row = [str(now.month) + '/' + str(now.day), time, '日本','日経','★★★','', '', '', pd.to_datetime(us_datetime), pd.to_datetime(from_us_datetime), pd.to_datetime(to_us_datetime)]
-        dfs1.loc[998] = row
+        # # 日経開始時間
+        # us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 20:00'
+        # from_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 19:00'
+        # to_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 21:00'
+        # time = datetime.time(9, 00, 00)
+        # row = [str(now.month) + '/' + str(now.day), '', '日本','日経','★★★','', '', '', pd.to_datetime(us_datetime), pd.to_datetime(from_us_datetime), pd.to_datetime(to_us_datetime)]
+        # dfs1.loc[998] = row
 
-        # ダウ開始時間
-        _market = market.condition.Market()
-        utc_time = _market.get_utc_time()
-        if _market.get_is_summer(utc_time):
-            us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 09:30'
-            from_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 08:30'
-            to_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 10:30'
-            time = datetime.time(22, 30, 00)
-        else:
-            us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 10:30'
-            from_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 09:30'
-            to_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 11:30'
-            time = datetime.time(23, 30, 00)
+        # # ダウ開始時間
+        # _market = market.condition.Market()
+        # utc_time = _market.get_utc_time()
+        # if _market.get_is_summer(utc_time):
+        #     us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 09:30'
+        #     from_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 08:30'
+        #     to_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 10:30'
+        #     time = datetime.time(22, 30, 00)
+        # else:
+        #     us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 10:30'
+        #     from_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 09:30'
+        #     to_us_datetime = str(now.year) +  '/' + str(now.month) + '/' + str(now.day) + ' 11:30'
+        #     time = datetime.time(23, 30, 00)
 
-        row = [str(now.month) + '/' + str(now.day), time, '米国','ダウ','★★★','', '', '', pd.to_datetime(us_datetime), pd.to_datetime(from_us_datetime), pd.to_datetime(to_us_datetime)]
-        dfs1.loc[999] = row
+        # row = [str(now.month) + '/' + str(now.day), '', '米国','ダウ','★★★','', '', '', pd.to_datetime(us_datetime), pd.to_datetime(from_us_datetime), pd.to_datetime(to_us_datetime)]
+        # dfs1.loc[999] = row
 
         return dfs1
 
@@ -112,7 +112,6 @@ class Calendar(object):
         return df
 
     def in_danger_time(self, df):
-        df = df[df['important'].str.contains('★★★')]
         now = pd.Timestamp.now()
         # 計算式
         # 答え < 0 or  0 > 0 - self.hours　危険時間帯
